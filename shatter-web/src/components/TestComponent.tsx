6b9b1538670a0afe2bd1a4a767064446bac849b5
Le Nguyen Quang Minh <101281380+lnqminh3003@users.noreply.github.com>
2025-10-27 16:40:20 -0600

    test trigger CI when having new PR

--- shatter-web/src/components/TestComponent.tsx
+++ shatter-web/src/components/TestComponent.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function TestComponent() {
   return (
     <>
-      <div className="bg-amber-200">
+      <div className="bg-amber-200 text-blue-700">
         <p>Hello!!!!!</p>
       </div>
     </>
