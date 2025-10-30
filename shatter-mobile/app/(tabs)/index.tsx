5ef0fdc7baf381e042cb03ee57058255cdc411b7
Le Nguyen Quang Minh <101281380+lnqminh3003@users.noreply.github.com>
2025-10-29 20:37:37 -0600

    create initial project for mobile

---
+++ shatter-mobile/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Image } from "expo-image";
+import { Platform, StyleSheet, View } from "react-native";
+
+import { Link } from "expo-router";
+
+export default function HomeScreen() {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({
+ 
+});
