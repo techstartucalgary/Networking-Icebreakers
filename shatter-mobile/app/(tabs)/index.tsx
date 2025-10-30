1087339d5f9f097a0db9d6e450656a9f2cf1b127
Le Nguyen Quang Minh <101281380+lnqminh3003@users.noreply.github.com>
2025-10-29 20:48:54 -0600

    add example for mobile

--- shatter-mobile/app/(tabs)/index.tsx
+++ shatter-mobile/app/(tabs)/index.tsx
@@ -1,12 +1,19 @@
 import { Image } from "expo-image";
-import { Platform, StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, View, Text } from "react-native";
 
 import { Link } from "expo-router";
 
 export default function HomeScreen() {
-  return <View></View>;
+  return <View>
+    <Text style={styles.title}>Hello</Text>
+  </View>;
 }
 
 const styles = StyleSheet.create({
- 
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
+  },
 });
