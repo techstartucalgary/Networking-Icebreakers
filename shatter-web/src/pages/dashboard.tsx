34253fcec7dcd5d81f2df7c7bd9048e9932901d5
youssef.ibrahim3 <youss@YOUSSEF-LAPTOP>
2025-10-30 19:21:52 -0600

    Add QR code component and page templates

---
+++ shatter-web/src/pages/dashboard.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import TestComponent from "../components/TestComponent.tsx";
+
+function Dashboard() {
+    return (
+        <>
+            <div className="bg-blue-100">
+                <p>Event Dashboard Coming Soon.</p>
+                <TestComponent />
+            </div>
+        </>
+    );
+}
+
+export default Dashboard;
