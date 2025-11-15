a97cd25831a8d508c77c38fc05de5ffbfc0fd1c3
Tahamina <73515136+tahaminachy43@users.noreply.github.com>
2025-11-14 23:48:30 -0700

    camera set up done
    changes made in camera set up done

--- shatter-mobile/app/(tabs)/JoinEvent.tsx
+++ shatter-mobile/app/(tabs)/JoinEvent.tsx
@@ -1,14 +1,25 @@
 import { Text, View } from "react-native";
 import { useAuth } from "../context/AuthContext";
+import { Button } from "react-native";
+import {useRouter} from "expo-router";
 
 export default function Profile() {
   const { user } = useAuth();
+  const router = useRouter();
+
 
   if (user) {
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Welcome, {user.name}!</Text>
         <Text>Join an Event Here</Text>
+        <Button
+          title ="Scan Qr Code"
+          onPress = {()=>{
+            router.push("../../src/components/QrcodeScan")
+          }}
+
+        />
         </View>
     );
   } else {
@@ -16,6 +27,14 @@ export default function Profile() {
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Welcome!</Text>
         <Text>Join an Event Here</Text>
+        {/* <Button>Scan Qr Code</Button> */}
+        <Button
+          title ="Scan Qr Code"
+          onPress = {()=>{
+            router.push("../../src/components/QrcodeScan")
+          }}
+
+        />
         </View>
     );
   }
