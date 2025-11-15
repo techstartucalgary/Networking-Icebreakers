39346d82daf83040c179ef9a2791fa23e52151cf
Le Nguyen Quang Minh <101281380+lnqminh3003@users.noreply.github.com>
2025-11-15 12:38:25 -0700

    remove comments in the code

--- shatter-backend/src/routes/user_route.ts
+++ shatter-backend/src/routes/user_route.ts
@@ -1,16 +1,9 @@
-// router is like a mini-express app that allows grouping of related routes together
 import { Router } from 'express';
 import { getUsers, createUser } from '../controllers/user_controller';
-// Importing controller functions that handle logic for each route
-// These function define what happens when a req is received
 
-// creating new router instance
 const router = Router();
 
-// Defining routes for the /api/users path
-router.get('/', getUsers);    // when GET req is made, run getUsers func
-router.post('/', createUser); // when POST req is made, run creatUser func
+router.get('/', getUsers);    
+router.post('/', createUser); 
 
-// Export the router so it can be used in app.ts
-// app.ts imports router and mounts it under '/api/users'
 export default router;
