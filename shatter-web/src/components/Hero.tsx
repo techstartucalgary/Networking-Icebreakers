0ecfefe4b3692368f1b9c6f0edd4b4fc33af63db
jktieh <jasontieh@gmail.com>
2026-02-01 19:42:27 -0700

    Adjust hero margin

--- shatter-web/src/components/Hero.tsx
+++ shatter-web/src/components/Hero.tsx
@@ -104,7 +104,7 @@ const Hero: React.FC<HeroProps> = ({ qrPayload = "hello" }) => {
       </div>
 
       {/* Main Content */}
-      <div className="container mx-auto px-4 relative z-10">
+      <div className="container mx-auto px-16 relative z-10">
         <div className="flex flex-col lg:flex-row items-center justify-between gap-12 lg:gap-16 max-w-7xl mx-auto">
           
           {/* Left Column - Text Content */}
