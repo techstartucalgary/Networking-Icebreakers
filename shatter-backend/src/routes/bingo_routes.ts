8730ab722ab87e00f4d38472e6a62297b3055ff9
Le Nguyen Quang Minh <101281380+lnqminh3003@users.noreply.github.com>
2026-02-06 21:44:43 -0700

    Update route for bingo game

--- shatter-backend/src/routes/bingo_routes.ts
+++ shatter-backend/src/routes/bingo_routes.ts
@@ -1,16 +1,16 @@
 import { Router } from 'express';
 import { createBingo, getBingo, updateBingo} from '../controllers/bingo_controller';
+import { authMiddleware } from '../middleware/auth_middleware';
 
 const router = Router();
 
-// POST /api/bingo/createEvent - create new event
-router.post('/createBingo', createBingo);
+router.post('/createBingo', authMiddleware, createBingo);
 
 // POST /api/bingo/getBingo - get bingo details
-router.post('/getBingo', getBingo);
+router.get('/getBingo/:id', getBingo);
 
 // POST /api/bingo/updateBingo - update bingo details
-router.put("/updateBingo", updateBingo);
+router.put("/updateBingo", authMiddleware, updateBingo);
 
 
 export default router;
