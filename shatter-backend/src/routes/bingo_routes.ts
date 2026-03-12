3278a1bd95dbaa83b5a5cf20700d8029d1d761df
Inko-z <rodolfoandresgp@hotmail.com>
2026-03-12 14:18:57 -0600

    added route for bingo game generation

--- shatter-backend/src/routes/bingo_routes.ts
+++ shatter-backend/src/routes/bingo_routes.ts
@@ -1,16 +1,19 @@
 import { Router } from 'express';
-import { createBingo, getBingo, updateBingo} from '../controllers/bingo_controller';
+import { createBingo, getBingo, updateBingo, generateBingo} from '../controllers/bingo_controller';
 import { authMiddleware } from '../middleware/auth_middleware';
 
 const router = Router();
 
 router.post('/createBingo', authMiddleware, createBingo);
 
-// POST /api/bingo/getBingo - get bingo details
+// GET /api/bingo/getBingo - get bingo details
 router.get('/getBingo/:eventId', getBingo);
 
-// POST /api/bingo/updateBingo - update bingo details
+// PUT /api/bingo/updateBingo - update bingo details
 router.put("/updateBingo", authMiddleware, updateBingo);
+
+// POST /api/bingo/generateBingo - generate bingo using AI for an event
+router.put("/generateBingo", generateBingo);
 
 
 export default router;
